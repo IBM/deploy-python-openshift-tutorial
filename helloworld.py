607978661e3cae3c2615c965484a4e8161970f7f
Pablo <pablo_ortega@es.ibm.com>
2022-10-06 16:16:02 +0200

    Changes

--- helloworld.py
+++ helloworld.py
@@ -1,16 +1,6 @@
-'''
-Created on 04-Sep-2019
-
-@author: bkadambi
-'''
-
-# -*- coding: UTF-8 -*-
-"""
-hello_flask: First Python-Flask webapp
-"""
-
 from prometheus_client import Counter
 from flask import Flask  # From module flask import class Flask
+from flask import Response 
 app = Flask(__name__)    # Construct an instance of Flask class for our webapp
 
 c_hellos = Counter('count_hellos', 'number of hellos')
@@ -18,14 +8,13 @@ c_byes = Counter('count_byes', 'number of byes')
 
 @app.route('/')   # URL '/' to be handled by main() route handler
 def main():
-    """Say hello"""
-    return 'Hello, world!'
+    return Response('Hello, world 2!', mimetype="text/xml")
 
 @app.route("/metrics") # URL for metrics
 def metrics():
     res = "c_hellos " + str(c_hellos._value.get()) + '\n'
     res = res + "c_byes " + str(c_byes._value.get())
-    return res
+    return Response(res, mimetype="text/xml")
 
 @app.route("/hello") 
 def hello():
