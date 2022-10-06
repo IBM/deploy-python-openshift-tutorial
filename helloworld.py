a936c083414a0b3ebbd0bd06d6bf290980f770b2
Pablo <pablo_ortega@es.ibm.com>
2022-10-06 13:06:30 +0200

    changges

--- helloworld.py
+++ helloworld.py
@@ -8,8 +8,13 @@ Created on 04-Sep-2019
 """
 hello_flask: First Python-Flask webapp
 """
+
+from prometheus_client import Counter
 from flask import Flask  # From module flask import class Flask
 app = Flask(__name__)    # Construct an instance of Flask class for our webapp
+
+c_hellos = Counter('count_hellos', 'number of hellos')
+c_byes = Counter('count_byes', 'number of byes')
 
 @app.route('/')   # URL '/' to be handled by main() route handler
 def main():
@@ -18,14 +23,18 @@ def main():
 
 @app.route("/metrics") # URL for metrics
 def metrics():
-    return "Metrics"
+    res = "c_hellos " + str(c_hellos._value.get()) + '\n'
+    res = res + "c_byes " + str(c_byes._value.get())
+    return res
 
 @app.route("/hello") 
 def hello():
+    c_hellos.inc()
     return "hello"
 
 @app.route("/bye")
 def goodbye():
+    c_byes.inc()
     return "Goodbye"
 
 if __name__ == '__main__':  # Script executed directly?
